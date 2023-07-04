274995c4d49c76cf89ae8a243926cceb2dc4e0be
demoreon <demoreon@gmail.com>
2023-07-04 21:48:44 +0100

    Returns lists of students

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,17 @@
+/**
+ * Filters Students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @author Ogundimu Ajibad
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
