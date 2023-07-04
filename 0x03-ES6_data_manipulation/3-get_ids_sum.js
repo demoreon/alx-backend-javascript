4e4a82a3092626873aa506c08261961f4109d90a
demoreon <demoreon@gmail.com>
2023-07-04 21:51:21 +0100

    Returns lists of students

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,19 @@
+/**
+ * Returns the sum of all the students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Ogundimu Ajibade
+ * @returns {Number}
+ */
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
