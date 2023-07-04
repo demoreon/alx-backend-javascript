e9c77626c7c31d746b018bc968b755ae20e1b715
demoreon <demoreon@gmail.com>
2023-07-04 22:10:48 +0100

    set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * New function using set
+ * @param {Array} array - The source array.
+ * @author Ogundimu Ajibade
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
